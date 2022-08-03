b60b9cfe4b6d11506bf4b414ef4148c236028178
Jakub Was <wasjakub.jw@gmail.com>
2022-08-03 14:26:24 +0200

    feat: #4 'Static Files'

--- studyproject/studyproject/settings.py
+++ studyproject/studyproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +121,18 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# App related
 STATIC_URL = '/static/'
+
+# If we have static files defined outside our app
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# URL that handles the media served from MEDIA_ROOT
+MEDIA_URL = '/images/'
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
