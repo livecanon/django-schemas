be9f6392311f10524575568df5f2bab9994777ab
Jakub Was <wasjakub.jw@gmail.com>
2022-08-04 20:40:06 +0200

    feat: #6 'User login, logout and flash messages'

--- studyproject/projects/views.py
+++ studyproject/projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
 # from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 
 
 def projects(request):
@@ -26,6 +27,7 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', context)
 
 
+@login_required(login_url='login')
 def createProject(request):
     form = ProjectForm()
 
@@ -41,6 +43,7 @@ def createProject(request):
     return render(request, 'projects/project_form.html', context)
 
 
+@login_required(login_url='login')
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     # see https://docs.djangoproject.com/en/3.2/topics/forms/modelforms/#the-save-method
@@ -58,6 +61,7 @@ def updateProject(request, pk):
     return render(request, 'projects/project_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
 
--- studyproject/users/views.py
+++ studyproject/users/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+# see https://docs.djangoproject.com/en/3.2/topics/auth/default/#the-login-required-decorator
+from django.contrib.auth.decorators import login_required
+# see https://docs.djangoproject.com/en/3.2/topics/auth/default/#the-loginrequired-mixin
+# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
+
+def loginUser(request):
+    # see https://docs.djangoproject.com/en/3.2/topics/auth/default/#how-to-log-a-user-in
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user) # creates a session
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+
+
+    return render(request, 'users/login_register.html')
+
+
+@login_required
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User was logged out')
+    return redirect('login')
 
 
 def profiles(request):
