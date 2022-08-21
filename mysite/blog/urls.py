fa0f8921536c8fc824435fd8e6650cdf6fbcb35b
Jakub Was <wasjakub.jw@gmail.com>
2022-08-21 21:20:25 +0200

    refactor: linting

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name='post_list'),
-    path("<int:id>/", views.post_detail, name='post_detail'),
-    path("create/", views.post_create, name='post_create'),
+    path("", views.post_list, name="post_list"),
+    path("<int:id>/", views.post_detail, name="post_detail"),
+    path("create/", views.post_create, name="post_create"),
 ]
