dc9a8d90baf628caf8ac3e98bf83ea526a923b58
wasjakub <wasjakub.jw@gmail.com>
2022-10-01 00:43:12 +0200

    fix: create user validation through api

--- core/users/views.py
+++ core/users/views.py
@@ -1,10 +1,10 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import UserSerializer
 
 
 class UserCreate(APIView):
@@ -12,12 +12,11 @@ class UserCreate(APIView):
 
     def post(self, request, format="json"):
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             user = serializer.save()
             if user:
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BlacklistTokenUpdateView(APIView):
