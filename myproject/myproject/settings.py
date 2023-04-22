b74508c472b4d6c37e8d6ac38833776539b816ea
Jakub Was <wasjakub.jw@gmail.com>
2023-04-22 14:03:06 +0200

    refactor: modify settings

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v-_k$ng81w))(!7!u^bx%&=do28#(shu0j%5%0a4jvm#o$pp4s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', [])
 
 
 # Application definition
